d38abf4534787a2b139134c17d34ee5b1c7b6bac
pablo <remagforce@gmail.com>
2020-08-17 09:03:19 -0600

    Version Inicial Programa prueba.

---
+++ SincronizacionDeHilos/src/hilos/Hilo1.java
@@ -0,0 +1,25 @@
+package hilos;
+
+public class Hilo1 extends Thread{
+	
+	@Override
+	public void run() {
+		
+		for(int i = 0; i <= 5; i++) {
+			
+			System.out.print(i + ": G");
+			try {
+				Hilo1.sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.print("erro en la clase hilo 1 " + e);
+			}
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+}
---
+++ SincronizacionDeHilos/src/hilos/Hilo2.java
@@ -0,0 +1,26 @@
+package hilos;
+
+public class Hilo2 extends Thread{
+	
+	
+	@Override
+	public void run() {
+		
+		for(int i = 0; i <= 5; i++) {
+			
+			System.out.print("E");
+			
+			try {
+				Hilo2.sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.print("erro en la clase hilo 2 " + e);
+			}
+			
+			
+			
+		}
+		
+		
+	}
+
+}
